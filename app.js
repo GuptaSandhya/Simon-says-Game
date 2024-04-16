d921914c395e2092e6918f5bc05e4dc82727aba7
sandhya <tech.eve27@gmail.com>
2024-04-16 17:05:01 +0530

    function modification

--- app.js
+++ app.js
@@ -1,94 +1,133 @@
-let gameSeq = [];
-let userSeq = [];
+let gameSeq =[];
+let userSeq =[];
 
-let btns = ["yellow","red","green","blue"];
+let btns = [ "red", "yellow" , "green", "purple"];
 
 let started = false;
 let level = 0;
 
-let h2 = document.querySelector("h2");
+let h2 =document.querySelector('h2');
 
-document.addEventListener("keypress", function(){
-    if(started==false){
-    console.log("Game Started");
-    started = true;
-
-    levelUp();
-
+//start game----------------
+let startbtn = document.querySelector(".start"); 
+startbtn.addEventListener("click",function(){        
+    if(started == false){
+        console.log("game is started");
+        started = true;   
+        startbtn.innerText = "End"; 
+        startbtn.style.backgroundColor = "red"   
+        levelUp();
+               
     }
+    // else{
+    //     startbtn.style.backgroundColor = "blue"   
+    //     startbtn.innerText = "Start";
+    //     reset()
+    // }
 });
-function gameFlash(btn) {
+
+
+//flaxh buttons & level up ----------------------
+function gameFlash(btn){
     btn.classList.add("flash");
-    setTimeout(function(){
+    setTimeout(() => {
         btn.classList.remove("flash");
-    }, 300);
+    }, 200);
 }
 
-function userFlash(btn) {
-    btn.classList.add("userflash");
-    setTimeout(function(){
-        btn.classList.remove("userflash");
-    }, 300);
+function userFlash(btn){
+    btn.classList.add("userFlash");
+    setTimeout(() => {
+        btn.classList.remove("userFlash");
+    }, 200);
 }
-
 
 function levelUp(){
     userSeq = [];
     level++;
     h2.innerText = `Level ${level}`;
-    
-    let randIdx = Math.floor(Math.random() * 3);     
+    let randIdx = Math.floor(Math.random() * 4);
+
     let randColor = btns[randIdx];
-    let randBtn = document.querySelector(`.${randColor}`);               //random btn choose
+    let randBtn = document.querySelector(`.${randColor}`);
     // console.log(randIdx);
     // console.log(randColor);
     // console.log(randBtn);
     gameSeq.push(randColor);
     console.log(gameSeq);
+
     gameFlash(randBtn);
 }
-
+// let currlevel = 0;
 function checkAns(idx){
-    //console.log("curr level :", level);
-    //let idx = level-1;        //fixed index
-
+    // console.log(`curr level : ${level}`);
     if(userSeq[idx]===gameSeq[idx]){
-        if(userSeq.length==gameSeq.length){
-            setTimeout(levelUp, 1000);            // levelUp();
+        // console.log("same value");
+        if(userSeq.length == gameSeq.length){
+            setTimeout(() => levelUp(), 1000);
         }
-        //console.log("same value");
-    }else{
-        h2.innerHTML = `Game Over! Your score is <b>${level}</b> <br>Press any key to start game.`;     //innerHTML: to add tags
-        reset();
-        document.querySelector("body").style.backgroundColor = "red";
-        setTimeout(function(){
-            document.querySelector("body").style.backgroundColor = "white";
-        },150);
     }
+    else{
+        h2.innerHTML = `Game Over! Your score was <b>${level}</b> <br>Press Start Button To Start`;
+        reset();
 
+        document.querySelector("body").style.backgroundColor  = "red";
+        // startbtn.style.backgroundColor = "blue";   
+
+        setTimeout(() => {
+            document.querySelector("body").style.backgroundColor  = "black";
+        }, 100);
+        // document.querySelector("body").style.backgroundColor  = "red";
+        // setTimeout(() => {
+        //     document.querySelector("body").style.backgroundColor  = "red";
+        // }, 200);
+        // setTimeout(() => {
+        //     document.querySelector("body").style.backgroundColor  = "black";
+        // }, 300);
+        // setTimeout(() => {
+        //     document.querySelector("body").style.backgroundColor  = "red";
+        // }, 400);
+        // setTimeout(() => {
+        //     document.querySelector("body").style.backgroundColor  = "black";
+        // }, 500);
+    
+        let highScore = document.querySelector('h1.highscore');
+        
+        if(currlevel >= level){
+            highScore.innerText = `Your High Score is ${currlevel}.`;
+        }else{
+            highScore.innerText = `Your High Score is ${level}.`;
+            currlevel = level;            
+        }
+        reset();
+    }
 }
 
-function btnPress() {
-    //console.log(this);              //optional
+//button event listeners ------------------------
+function btnPress(){
+    // console.log("btn was pressed"); 
+    // console.log(this);
     let btn = this;
     userFlash(btn);
-
-    userColor = btn.getAttribute("id");
+    // let userColor = this.id;
+    let userColor = this.getAttribute("id");
+    console.log(userColor);
     userSeq.push(userColor);
-
-    checkAns(userSeq.length-1);         //current index
+    console.log(userSeq);
+    
+    checkAns(userSeq.length-1);
+}
+let allBtns = document.querySelectorAll('.btn');
+for (btn of allBtns) {
+    btn.addEventListener("click",btnPress);
 }
 
-let allBtns = document.querySelectorAll(".btn");
-for(btn of allBtns){
-    btn.addEventListener("click", btnPress);
-}
-
-function reset() {
+function reset(){
     started = false;
     gameSeq = [];
     userSeq = [];
     level = 0;
+    // startbtn.innerText = "Start"
+
+    // highScore.innerText ="";  
 }
-
-
